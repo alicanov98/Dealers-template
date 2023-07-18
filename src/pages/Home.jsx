2a695228e79d7fa6239840f013353b3bb3e81b2d
JavidanGurbanli <cavidanqurbanli2003@gmail.com>
2023-07-18 19:17:35 +0400

    feat: added products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,12 @@ import NewCollection from "./sections/NewCollection";
 import Woman from '../assets/images/model_woman_1.png.webp';
 import Man from '../assets/images/model_5.png.webp';
 import HomeHero from "./sections/HomeHero";
+import Products from "./sections/Products";
 
 const Home = () => {
   return <div>
     <HomeHero />
+    <Products/>
     <NewCollection src={Woman} desc="Jacket"/>
     <NewCollection src={Man} desc="NEW DENIM COAT"/>
   </div>
---
+++ src/pages/sections/Products.jsx
@@ -0,0 +1,19 @@
+import data from '../../assets/data/data.json';
+import "../../assets/scss/components/_products.scss"
+const Products = () => {
+  return (
+    <section className='products'>
+        {data.products.map((product, index) => (
+        <div key={index} className='product'>
+          {product.title === "Marc Jacobs Bag" ? <h4>SALE</h4> : ""}
+          <div className='product_img'><img src={product.imgSrc} alt={product.title} /></div>
+          <h3>{product.title}</h3>
+          <h2>{product.text}</h2>
+          <p>{product.price} {product.title === "Marc Jacobs Bag" ? <span>$30.00</span> : "" }</p>
+        </div>
+      ))}
+    </section>
+  )
+}
+
+export default Products
